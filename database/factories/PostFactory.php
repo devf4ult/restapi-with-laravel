48497a03ffaa34c2e4d72ca1f686cc92e7ebaba7
runtime3rr0r <devf4ult@gmail.com>
2021-10-27 16:40:07 +0700

    CRUD RestAPI Laravel.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Post;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    protected $model = Post::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(50),
+            'body' => $this->faker->text(300)
+        ];
+    }
+}
